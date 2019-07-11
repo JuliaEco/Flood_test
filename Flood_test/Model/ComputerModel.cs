0a1ed926eac546015ee37898eaf90c947f2cc04b
eless <solovey.al@gmail.com>
2019-07-12 01:55:29 +0300

    run Compute

--- Flood_test/Model/ComputerModel.cs
+++ Flood_test/Model/ComputerModel.cs
@@ -1,13 +1,42 @@
 ﻿using Presenter;
 using System;
+using System.Diagnostics;
 
 namespace Model
 {
     public class ComputerModel : IComputeModel
     {
+        private string SuccessMessage = "Successfully computed";
         public string Compute()
         {
-            throw new NotImplementedException();
+            var result = RunComputer();
+            return result;
+            //throw new NotImplementedException();
+        }
+
+        private string RunComputer()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                CreateNoWindow = false,
+                UseShellExecute = false,
+                FileName = "..\\..\\..\\Computer\\11lisfloodRelease_double.exe",
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+            //startInfo.Arguments = "-f j -o \"" + ex1 + "\" -z 1.0 -s y " + ex2;
+
+            try
+            {
+                using (var exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+                    return SuccessMessage;
+                }
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
         }
     }
 }
