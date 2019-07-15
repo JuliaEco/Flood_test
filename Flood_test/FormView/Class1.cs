7003e82c2e08668feed2149d83c6a0d57be40816
JuliaEco <iuliiashustikova@gmail.com>
2019-07-15 18:19:28 +0200

    classes added

---
+++ Flood_test/FormView/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flood_test
+{
+    [Serializable]
+    public class floodDataParameters
+    {
+        public string DEMfile;
+        public string rootdir;
+        public string dirroot;
+        public string sim_time;
+        public string initial_tstep;
+        public string massint;
+        public string saveint;
+        public string fpfric;
+        public string bcifile;
+        public string bdyfile;
+        public string adaptoff;
+        public string sgc_enable;
+        public string drycheckon;
+        public string elevoff;
+        public string max_Froude;
+
+    }
+}
